7fa11882025a8451ef8c3e48aa7ef143d72aef08
Gabriele Santomaggio <g.santomaggio@gmail.com>
2024-05-13 09:01:13 +0200

    Add utils classes
    Signed-off-by: Gabriele Santomaggio <g.santomaggio@gmail.com>

--- RabbitMQ.AMQP.Client/ByteCapacity.cs
+++ RabbitMQ.AMQP.Client/ByteCapacity.cs
@@ -4,13 +4,13 @@ namespace RabbitMQ.AMQP.Client;
 
 public partial class ByteCapacity : IEquatable<ByteCapacity>
 {
-    public long Bytes { get; }
+    private long _bytes;
     private string _input;
 
     private ByteCapacity(long bytes, string input)
     {
         _input = input;
-        Bytes = bytes;
+        _bytes = bytes;
     }
 
     private ByteCapacity(long bytes) : this(bytes, bytes.ToString())
@@ -49,17 +49,7 @@ public partial class ByteCapacity : IEquatable<ByteCapacity>
     }
 
 
-    // private static int KILOBYTES_MULTIPLIER = 1000;
-
-    // private static final Pattern PATTERN =
-    // Pattern.compile("^(?<size>\\d*)((?<unit>kb|mb|gb|tb))?$", Pattern.CASE_INSENSITIVE);
-
-    // private const string Pattern = @"(?i)^(?<size>\\d*)((?<unit>kb|mb|gb|tb))?$";
     private static readonly Regex SizeRegex = new Regex(@"^(\d+)([kKmMgGtTpP]?[bB]?)$", RegexOptions.Compiled);
-
-    // public static ByteCapacity from(String value) {
-    //     Matcher matcher = PATTERN.matcher(value);
-    // }
 
     public static ByteCapacity From(string value)
     {
@@ -84,118 +74,14 @@ public partial class ByteCapacity : IEquatable<ByteCapacity>
 
     public long ToBytes()
     {
-        return Bytes;
+        return _bytes;
     }
-
-
-// Create a Regex
-    // Regex rg = new Regex(pattern);
-
-    // private static final String UNIT_MB = "mb";
-    // private static final int KILOBYTES_MULTIPLIER = 1000;
-    // private static final int MEGABYTES_MULTIPLIER = 1000 * 1000;
-    // private static final int GIGABYTES_MULTIPLIER = 1000 * 1000 * 1000;
-    // private static final long TERABYTES_MULTIPLIER = 1000L * 1000L * 1000L * 1000L;
-    //
-    // private static final Pattern PATTERN =
-    //     Pattern.compile("^(?<size>\\d*)((?<unit>kb|mb|gb|tb))?$", Pattern.CASE_INSENSITIVE);
-    // private static final String GROUP_SIZE = "size";
-    // private static final String GROUP_UNIT = "unit";
-    // private static final String UNIT_KB = "kb";
-    // private static final String UNIT_GB = "gb";
-    // private static final String UNIT_TB = "tb";
-    //
-    // private static final Map<String, BiFunction<Long, String, ByteCapacity>> CONSTRUCTORS =
-    //     Collections.unmodifiableMap(
-    //         new HashMap<String, BiFunction<Long, String, ByteCapacity>>() {
-    //           {
-    //             put(UNIT_KB, (size, input) -> ByteCapacity.kB(size, input));
-    //             put(UNIT_MB, (size, input) -> ByteCapacity.MB(size, input));
-    //             put(UNIT_GB, (size, input) -> ByteCapacity.GB(size, input));
-    //             put(UNIT_TB, (size, input) -> ByteCapacity.TB(size, input));
-    //           }
-    //         });
-    //
-    // private final long bytes;
-    // private final String input;
-    //
-    // private ByteCapacity(long bytes) {
-    //   this(bytes, String.valueOf(bytes));
-    // }
-    //
-    // private ByteCapacity(long bytes, String input) {
-    //   this.bytes = bytes;
-    //   this.input = input;
-    // }
-    //
-    // public static ByteCapacity B(long bytes) {
-    //   return new ByteCapacity(bytes);
-    // }
-    //
-    // public static ByteCapacity kB(long kilobytes) {
-    //   return new ByteCapacity(kilobytes * KILOBYTES_MULTIPLIER);
-    // }
-    //
-    // public static ByteCapacity MB(long megabytes) {
-    //   return new ByteCapacity(megabytes * MEGABYTES_MULTIPLIER);
-    // }
-    //
-    // public static ByteCapacity GB(long gigabytes) {
-    //   return new ByteCapacity(gigabytes * GIGABYTES_MULTIPLIER);
-    // }
-    //
-    // public static ByteCapacity TB(long terabytes) {
-    //   return new ByteCapacity(terabytes * TERABYTES_MULTIPLIER);
-    // }
-    //
-    // private static ByteCapacity kB(long kilobytes, String input) {
-    //   return new ByteCapacity(kilobytes * KILOBYTES_MULTIPLIER, input);
-    // }
-    //
-    // private static ByteCapacity MB(long megabytes, String input) {
-    //   return new ByteCapacity(megabytes * MEGABYTES_MULTIPLIER, input);
-    // }
-    //
-    // private static ByteCapacity GB(long gigabytes, String input) {
-    //   return new ByteCapacity(gigabytes * GIGABYTES_MULTIPLIER, input);
-    // }
-    //
-    // private static ByteCapacity TB(long terabytes, String input) {
-    //   return new ByteCapacity(terabytes * TERABYTES_MULTIPLIER, input);
-    // }
-    //
-    // public long toBytes() {
-    //   return bytes;
-    // }
-    //
-    // public static ByteCapacity from(String value) {
-    //   Matcher matcher = PATTERN.matcher(value);
-    //   if (matcher.matches()) {
-    //     long size = Long.valueOf(matcher.group(GROUP_SIZE));
-    //     String unit = matcher.group(GROUP_UNIT);
-    //     ByteCapacity result;
-    //     if (unit == null) {
-    //       result = ByteCapacity.B(size);
-    //     } else {
-    //       return CONSTRUCTORS
-    //           .getOrDefault(
-    //               unit.toLowerCase(),
-    //               (v, input) -> {
-    //                 throw new IllegalArgumentException("Unknown capacity unit: " + unit);
-    //               })
-    //           .apply(size, value);
-    //     }
-    //     return result;
-    //   } else {
-    //     throw new IllegalArgumentException("Cannot parse value for byte capacity: " + value);
-    //   }
-
-
-    // }
 
 
     public bool Equals(ByteCapacity? other)
     {
-        throw new NotImplementedException();
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return _bytes == other._bytes;
     }
 }
--- Tests/ByteCapacityTests.cs
+++ Tests/ByteCapacityTests.cs
@@ -16,7 +16,7 @@ public class ByteCapacityTests
     [InlineData("1gb", 1000 * 1000 * 1000)]
     [InlineData("28GB", 28 * 1000L * 1000L * 1000L)]
     [InlineData("1tb", 1000L * 1000L * 1000L * 1000L)]
-    [InlineData("1TB", 1000L * 1000L * 1000L * 1000L)]
+    [InlineData("23TB", 23 * 1000L * 1000L * 1000L * 1000L)]
     public void FromShouldReturnTheCorrectBytesValues(string input, long expectedBytes)
     {
         Assert.Equal(expectedBytes, ByteCapacity.From(input).ToBytes());
@@ -27,11 +27,30 @@ public class ByteCapacityTests
     {
         Assert.Throws<ArgumentException>(() => ByteCapacity.From("invalid"));
     }
-    
-    // [Fact]
-    // public void ()
-    // {
-    //     Assert.Throws<ArgumentException>(() => ByteCapacity.From("1"));
-    // }
 
+    [Fact]
+    public void ByteCapacityShouldReturnTheValidBytes()
+    {
+        Assert.Equal(99, ByteCapacity.B(99).ToBytes());
+        Assert.Equal(76000, ByteCapacity.Kb(76).ToBytes());
+        Assert.Equal(789 * 1000 * 1000, ByteCapacity.Mb(789).ToBytes());
+        Assert.Equal(134 * 1000L * 1000L * 1000L, ByteCapacity.Gb(134).ToBytes());
+        Assert.Equal(12 * 1000L * 1000L * 1000L * 1000L, ByteCapacity.Tb(12).ToBytes());
+    }
+
+    [Theory]
+    [InlineData(9, 9 * 1000)]
+    [InlineData(76, 76 * 1000)]
+    public void KbByteCapacityShouldBeEqualToB(long value, long expectedBytes)
+    {
+        Assert.Equal(ByteCapacity.Kb(value), ByteCapacity.B(expectedBytes));
+    }
+
+    [Theory]
+    [InlineData(9, 9 * 1000L * 1000L * 1000L)]
+    [InlineData(76, 76 * 1000L * 1000L * 1000L)]
+    public void GbByteCapacityShouldBeEqualToB(long value, long expectedBytes)
+    {
+        Assert.Equal(ByteCapacity.Gb(value), ByteCapacity.B(expectedBytes));
+    }
 }
