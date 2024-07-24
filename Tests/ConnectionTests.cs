ea902c733099882a1260c6af1ddaeca551f9b7a1
Luke Bakken <luke@bakken.io>
2024-07-24 15:20:27 -0700

    * Add debug output to show current dir for test.

--- Tests/ConnectionTests.cs
+++ Tests/ConnectionTests.cs
@@ -109,6 +109,7 @@ public class ConnectionTests
     public async Task ConnectUsingTlsAndClientCertificate()
     {
         string cwd = Directory.GetCurrentDirectory();
+        _output.WriteLine("@@@@ CURRENT DIRECTORY: {0}", cwd);
         string clientCertFile = Path.GetFullPath(Path.Join(cwd, "../../../../.ci/certs/client_localhost.p12"));
         Assert.True(File.Exists(clientCertFile));
         var cert = new X509Certificate2(clientCertFile, "grapefruit");
