e87f0b2f0d3dc41a7652862caabae4204b5f9856
Gabriele Santomaggio <g.santomaggio@gmail.com>
2024-07-25 11:41:54 +0200

    add script release
    Signed-off-by: Gabriele Santomaggio <g.santomaggio@gmail.com>

---
+++ create_tag.sh
@@ -0,0 +1,13 @@
+# /bin/bash
+version=$1
+# Regex pattern for the valid strings
+regex="^v([0-9]+)\.([0-9]+)\.([0-9]+)(-(alpha|beta|rc)\.([0-9]+))?$"
+  
+if [[ $version =~ $regex ]]; then
+    echo "Creating tag with: " $1
+    git tag -a -s -u GPG_KEY_ID -m 'rabbitmq-amqp-dotnet-client $1' '$1' && git push && git push --tags
+       echo "Tag created: " $1
+else
+    echo "Invalid version" $1
+fi
+
