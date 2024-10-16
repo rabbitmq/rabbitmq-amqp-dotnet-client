7cfdcbbc086d3b8307dc3a1ce2b7c2e4a5ef4141
Gabriele Santomaggio <G.santomaggio@gmail.com>
2024-10-16 15:47:57 +0200

    Add documentation (#79)
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- README.md
+++ README.md
@@ -1,22 +1,26 @@
 # RabbitMQ AMQP 1.0 .NET Client
 
 This library is meant to be used with RabbitMQ 4.0.
-Still work in progress suitable for testing in pre-production environments
+Suitable for testing in pre-production environments
+
+
+## Install
+
+The client is distributed via [NuGet](https://www.nuget.org/packages/RabbitMQ.AMQP.Client/).
+
+## Examples
+
+Inside the `docs/Examples` directory you can find examples of how to use the client.
+
+
+## Documentation
+
+- [Client Guide](https://www.rabbitmq.com/client-libraries/amqp-client-libraries)
+- [API](https://rabbitmq.github.io/rabbitmq-amqp-dotnet-client/api/RabbitMQ.AMQP.Client.html)
+
 
 ## How to Run
 
 - Start the broker with `./.ci/ubuntu/one-node/gha-setup.sh start`. Note that this has been tested on Ubuntu 22 with docker.
 - Run the tests with ` dotnet test ./Build.csproj  --logger "console;verbosity=detailed"`
 - Stop RabbitMQ with `./.ci/ubuntu/one-node/gha-setup.sh stop`
-
-## Getting Started
-
-You can find an example in: `docs/Examples/GettingStarted`
-
-## Install
-
-The client is distributed via [NuGet](https://www.nuget.org/packages/RabbitMQ.AMQP.Client/).
-
-## Documentation
-
-- [API](https://rabbitmq.github.io/rabbitmq-amqp-dotnet-client/api/RabbitMQ.AMQP.Client.html)
--- RabbitMQ.AMQP.Client/IRpcClient.cs
+++ RabbitMQ.AMQP.Client/IRpcClient.cs
@@ -10,23 +10,89 @@ namespace RabbitMQ.AMQP.Client
         IRpcClientBuilder RpcClient();
     }
 
+    /// <summary>
+    /// IRpcClientBuilder is the interface for creating an RPC client.
+    /// See also <seealso cref="IRpcClient"/> and <seealso cref="IRpcServerBuilder"/>
+    /// </summary>
     public interface IRpcClientBuilder
     {
+        /// <summary>
+        /// Request address where the client sends requests.
+        /// The server consumes requests from this address.
+        /// </summary>
+        /// <returns></returns>
         IRpcClientAddressBuilder RequestAddress();
+
+        /// <summary>
+        /// The queue from which requests are consumed.
+        /// if not set the client will create a temporary queue.
+        /// </summary>
+        /// <param name="replyToQueueName"> The queue name</param>
+        /// <returns></returns>
         IRpcClientBuilder ReplyToQueue(string replyToQueueName);
 
         IRpcClientBuilder ReplyToQueue(IQueueSpecification replyToQueue);
+
+        /// <summary>
+        /// Extracts the correlation id from the request message.
+        /// each message has a correlation id that is used to match the request with the response.
+        /// There are default implementations for the correlation id extractor.
+        /// With this method, you can provide a custom implementation.
+        /// </summary>
+        /// <param name="correlationIdExtractor"></param>
+        /// <returns></returns>
         IRpcClientBuilder CorrelationIdExtractor(Func<IMessage, object>? correlationIdExtractor);
 
+        /// <summary>
+        /// Post processes the reply message before sending it to the server.
+        /// The object parameter is the correlation id extracted from the request message.
+        /// There are default implementations for the reply post processor that use the correlationId() field
+        /// to set the correlation id of the reply message.
+        /// With this method, you can provide a custom implementation.
+        /// </summary>
+        /// <param name="requestPostProcessor"></param>
+        /// <returns></returns>
         IRpcClientBuilder RequestPostProcessor(Func<IMessage, object, IMessage>? requestPostProcessor);
 
+        /// <summary>
+        /// Client and Server must agree on the correlation id.
+        /// The client will provide the correlation id to send to the server.
+        /// If the default correlation id is not suitable, you can provide a custom correlation id supplier.
+        /// Be careful to provide a unique correlation id for each request. 
+        /// </summary>
+        /// <param name="correlationIdSupplier"></param>
+        /// <returns></returns>
+
         IRpcClientBuilder CorrelationIdSupplier(Func<object>? correlationIdSupplier);
+
+        /// <summary>
+        /// The time to wait for a reply from the server.
+        /// </summary>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
         IRpcClientBuilder Timeout(TimeSpan timeout);
+        /// <summary>
+        ///  Build and return the RPC client.
+        /// </summary>
+        /// <returns></returns>
         Task<IRpcClient> BuildAsync();
     }
 
+    /// <summary>
+    ///  IRpcClient is the interface for an RPC client.
+    /// See also <seealso cref="IRpcServer"/> and <seealso cref="IRpcClientBuilder"/>
+    /// </summary>
     public interface IRpcClient : ILifeCycle
     {
+        /// <summary>
+        /// PublishAsync sends a request message to the server and blocks the thread until the response is received.
+        /// The PublishAsync is thread-safe and can be called from multiple threads.
+        /// The Function returns the response message.
+        /// If the server does not respond within the timeout, the function throws a TimeoutException.
+        /// </summary>
+        /// <param name="message"> The request message</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns></returns>
         Task<IMessage> PublishAsync(IMessage message, CancellationToken cancellationToken = default);
     }
 }
